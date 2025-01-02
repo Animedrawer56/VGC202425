972a5c02144a13c9765cd3f10b8f0b29385dcb83
Animedrawer56 <brishep2022@gmail.com>
2025-01-02 16:46:39 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -43,7 +43,7 @@ export var soundArray = []; //list of sounds loaded
 
 //Here is where all the Sound File Players Start
 
-export var wallSound = new soundFile("slience.mp3"); //load sound
+export var wallSound = new soundFile("ahhhhhhh sound.mp3"); //load sound
 soundArray.push(wallSound); //add sound to list of sounds
 
 export var paddleSound = new soundFile("hit sound.mp3");
